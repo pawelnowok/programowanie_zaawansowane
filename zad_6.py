57a09e33505281e1d9af7c1e38e226c513692553
pawelnowok <115508643+pawelnowok@users.noreply.github.com>
2022-10-18 18:42:03 +0200

    Delete zad_6.py

--- zad_6.py
+++
@@ -1,7 +0,0 @@
-def check(lista1: list, lista2: list) -> list:
-    lista3 = set(lista1 + lista2)
-    return lista3
-
-sprawdzenie = check([1, 2, 3, 4, 5], [1, 4, 6, 7, 8, 9])
-
-print(sprawdzenie)
