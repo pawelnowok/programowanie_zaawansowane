41ddb52a9d36b21f58c7566988cccebfb07c674c
pawelnowok <115508643+pawelnowok@users.noreply.github.com>
2023-01-29 22:41:41 +0100

    Delete zad_4.py

--- zad_4.py
+++
@@ -1,9 +0,0 @@
-def suma(x: int, y: int, z: int) -> bool:
-    return x + y >= z
-
-sprawdzenie = suma(78, 35, 273)
-
-if sprawdzenie == True:
-    print("Suma liczb x i y jest większa lub równa liczbie z")
-else:
-    print("Suma liczb x i y jest mniejsza od liczby z")
