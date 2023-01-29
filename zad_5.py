f4fbdd6f43faea2e2abce86ec684cfe1e1b6c980
pawelnowok <115508643+pawelnowok@users.noreply.github.com>
2023-01-29 22:41:50 +0100

    Delete zad_5.py

--- zad_5.py
+++
@@ -1,9 +0,0 @@
-def check(lista: list, x: int) -> bool:
-    return x in lista
-
-sprawdzenie = check([1, 2, 3, 4, 5, 6, 7, 8, 9], 12)
-
-if sprawdzenie == True:
-    print("Element x należy do listy")
-else:
-    print("Element x nie należy do listy")
